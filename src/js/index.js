e348f10a9ebd6769d054db162efca74209358eab
Jeovan Delavechea <jeovan.delavechea88@gmail.com>
2022-12-29 14:29:37 -0300

    Exercício proposto nos módulos de css e jsavançado

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const botaoDeMudanca = document.getElementById('botao')
+const idDoConselho = document.querySelector('.aviso')
+const descricaoDoConselho = document.querySelector('.descricao')
+
+
+botaoDeMudanca.addEventListener('click', () =>{
+    return selecionarPeloId()
+})
+
+async function criarGeradorDeConselhos(){
+    const url = 'https://api.adviceslip.com/advice'
+    const resposta = await fetch(url)
+    return await resposta.json() 
+}
+
+async function selecionarPeloId(){
+    const geradorDeConselho = await criarGeradorDeConselhos()
+    const idEDescricao = await geradorDeConselho.slip
+    
+    idDoConselho.innerText = `ADVICE #${idEDescricao.id}`
+    
+    descricaoDoConselho.innerText = `"${idEDescricao.advice}"`
+}
+selecionarPeloId()
+
+
+    
